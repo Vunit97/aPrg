83b714322e5f71edbc937f3a5a59087c5e7d09f6
Jimmy Cu <Jimmy Cu>
2017-10-12 20:49:54 +0200

    Erfolgreich

--- benutzerverwaltung/server.js
+++ benutzerverwaltung/server.js
@@ -27,3 +27,119 @@ app.get('/', (request, response) =>{
 	response.sendfile(__dirname + '/index.html');
 });
 
+//Session
+const session = require("express-session");
+app.use(session({
+	secret: "example",
+	resave: false,
+	saveUninitialized: true,
+}));
+
+app.post("/anmelden", (request,response) => {
+	const nutzer = request.body["nutzer"];
+	const passwort = request.body["passwort"];
+	var accExtists = false;
+
+	//Ein User suchen
+	db.collection(DB_COLLECTION).findOne({'nutzer': nutzer}, (error,result) => {
+
+		// If account is found in databank
+		if(result != null) {
+			accExtists = true;	
+			console.log(result.passwort);
+
+			// if given password matches with account username
+			if (result.passwort == passwort) {
+				request.session['authenticated'] = true;
+				request.session["nutzer"] = nutzer;
+				response.redirect("/inside");
+
+			// wrong password given
+			} else {
+				console.log(error);
+				message = "Falsches Passwort dude...";
+				response.redirect("/");
+			}			
+		} 
+
+		// username is not in the databank
+		else {
+			message = "Uknown User...";
+			response.redirect("/");
+		}
+	});
+
+
+
+
+});
+
+app.get("/inside", (request, response) => {
+	if(request.session['authenticated'] == true) {
+		const nutzer = request.session['nutzer'];
+		response.render("inside", {"user": nutzer });
+	} else {
+		message = "No permission! Please login.";
+		response.redirect("/")
+	}
+
+});
+
+app.get("/logout", (request,response) => {
+	delete request.session.authenticated;
+	message = "Logout successful."
+	response.redirect("/");
+});
+
+app.get("/register", (request, response) => {
+	response.render('register', {"error": "","on": ""});
+});
+
+
+app.post("/registerverify", (request, response) => {
+	const nutzer = request.body["nutzer"];
+	const passwort = request.body["passwort"];
+	const passwordrepeat = request.body["passwordrepeat"];
+	const email = request.body["email"];
+	const error = [];
+
+	if(nutzer == "" || nutzer == null) {
+		error.push("Type a username!");
+	}
+	if(passwort == "" || passwort == null){
+		error.push("Type a password!");
+	} else {
+		if(passwordrepeat == "" || passwordrepeat == null) {
+			error.push("Don't forget to repeat your password!")
+		}
+	}
+	
+	if(email == "" || email == null || !email.includes("@")) {
+		error.push("Type a correct Email adress!")
+	}
+
+	if(passwort != passwordrepeat) {
+		error.push("Passwords dont match!")
+	}
+
+	// USER DATEN IN DATENBANK SPEICHERN 
+
+
+	if(error.length == 0) {
+		const on = "Succesfully registered!";
+		const documents = {'nutzer': nutzer, 'passwort': passwort, 'email': email};
+
+		db.collection(DB_COLLECTION).save(documents, (err, result) =>  {
+			if(err) return console.log(err);
+			console.log("saved to database");
+		});
+
+		response.render('register', {"error": error, "on": on});
+	} else {
+		response.render('register', {"error": error, 'on': ""});
+	}
+
+	
+});
+
+
